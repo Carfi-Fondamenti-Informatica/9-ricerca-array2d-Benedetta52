4ada1ad61ad7ad9d0a35c213be2c2acbfe3edaf1
Benedetta52 <91663828+Benedetta52@users.noreply.github.com>
2021-12-11 11:15:43 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -3,7 +3,7 @@
 
 bool ricerca(char lista [10][20], char nome[1][20], int &pos){
   
-  for (int i=; i<10; i++){
+  for (int i=0; i<10; i++){
     if (strcmp (lista [i], nome [0]) == 0){
       pos = i;
       return true;
